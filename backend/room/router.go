167a23caa53dc50178a885125f2c54505ee0abd5
lareii <lareii@proton.me>
2026-07-30 14:15:53 +0300

    chore: clean up project to setup git flow

--- backend/room/router.go
+++
@@ -1,9 +0,0 @@
-package room
-
-import "github.com/gofiber/fiber/v3"
-
-func Register(api fiber.Router) {
-	room := api.Group("/room")
-	room.Get("/create", createRoom)
-	room.Get("/join", joinRoom)
-}
