23e7f30f7bde3e23e8b60723b4280a70e8f94540
lareii <lareii@proton.me>
2026-08-08 16:53:19 +0300

    feat: add api client and auth endpoints

---
+++ frontend/lib/api/auth.ts
@@ -0,0 +1,49 @@
+import { apiClient } from "@/lib/api/client";
+
+export interface IntraResponse {
+  isNewUser: boolean;
+  accessToken?: string;
+  name?: string;
+}
+
+export const authService = {
+  signUp: (username: string, password: string) => {
+    return apiClient<{ accessToken: string }>("/auth/register", {
+      method: "POST",
+      body: { username, password }
+    });
+  },
+
+  signIn: (username: string, password: string) => {
+    return apiClient<{ accessToken: string }>("/auth/login", {
+      method: "POST",
+      body: { username, password }
+    });
+  },
+
+  signOut: () => {
+    return apiClient("/auth/logout", {
+      method: "POST"
+    });
+  },
+
+  intraLogin: () => {
+    return apiClient<{ url: string }>("/auth/42/login", {
+      method: "GET"
+    });
+  },
+
+  intraCallback: (code: string) => {
+    return apiClient<IntraResponse>("/auth/42/callback", {
+      method: "POST",
+      body: { code }
+    });
+  },
+
+  intraComplete: (accessToken: string, username: string) => {
+    return apiClient<{ accessToken: string }>("/auth/42/register", {
+      method: "POST",
+      body: { accessToken, username }
+    });
+  }
+};
