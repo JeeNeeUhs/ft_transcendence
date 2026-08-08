1c8075634f1da708b9ea667d649ddd6c2a7a7558
lareii <lareii@proton.me>
2026-08-08 15:05:47 +0300

    chore: add ui components with configurations

--- frontend/providers/index.tsx
+++ frontend/providers/index.tsx
@@ -1,9 +1,13 @@
+import { Toaster } from "@/components/ui/toast";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+      <Toaster />
+    </>
   );
 }
