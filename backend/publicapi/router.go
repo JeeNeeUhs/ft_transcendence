b643efeaa2cfaabf0059d0dc61b5d2bf7226d22e
lareii <lareii@proton.me>
2026-09-07 15:40:10 +0300

    hotfix: generate swagger docs in build-time

--- backend/publicapi/router.go
+++ backend/publicapi/router.go
@@ -1,31 +1,29 @@
 package publicapi
 
 import (
-	"os"
+	"net/url"
 
 	"github.com/JeeNeeUhs/ft_transcendence/apikey"
+	"github.com/JeeNeeUhs/ft_transcendence/config"
 	"github.com/JeeNeeUhs/ft_transcendence/docs"
 	"github.com/gofiber/contrib/v3/swaggo"
 	"github.com/gofiber/fiber/v3"
 )
 
 func Register(api fiber.Router) {
-
-	if envHost := os.Getenv("SWAGGER_HOST"); envHost != "" {
-		docs.SwaggerInfo.Host = envHost;
-	}
+	swaggerURL, _ := url.Parse(config.ApiURL)
+	docs.SwaggerInfo.Host = swaggerURL.Host
+	docs.SwaggerInfo.Schemes = []string{swaggerURL.Scheme}
 
 	publicGroup := api.Group("/public")
 
 	publicGroup.Get("/docs/*", swaggo.HandlerDefault)
 
-	
 	protected := publicGroup.Group("", apikey.APIKeyAuth, apikey.RateLimiter())
 
 	protected.Get("/stats/users", GetUserStatsHandler)
 	protected.Get("/stats/matches", GetMatchStatsHandler)
 
-	
 	protected.Get("/me/friends", GetFriendsHandler)
 	protected.Post("/me/friend-requests", SendFriendRequestHandler)
 	protected.Put("/me/friend-requests/:username", AcceptFriendRequestHandler)
